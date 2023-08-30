b2cb7fc8db4e1db6503ea9738a0ad658f6f49bde
josue <josuejero@hotmail.com>
2023-08-30 13:40:26 -0400

    changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f7972a3f7681203"; }
+/******/ 		__webpack_require__.h = function() { return "bccf2b2fe0f9956f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import "../styles/_header.scss";
+import Home from "../../public/home.png"
+import About from "../../public/about.png"
+import Blog from "../../public/blog.png"
+import Contact from "../../public/contact.png"
+import Image from "next/image";
 
 const Header = ({ darkMode, setDarkMode }) => (
   <nav className={`sidebar ${darkMode ? "dark" : ""}`}>
-    <h1 className="logo">developed-by-josue</h1>
+    {/* <h1 className="logo">developed-by-josue</h1> */}
     <ul className="image">
       <li>
         <BsFillMoonStarsFill
           onClick={() => setDarkMode(!darkMode)}
           className="dark-mode-icon"
         />
+      </li>
+      <li>
+        <Image src={Home}/>
+      </li>
+      <li>
+        <Image src={About}/>
+      </li>
+      <li>
+        <Image src={Blog}/>
+      </li>
+      <li>
+        <Image src={Contact}/>
       </li>
       <li>
         <a className="resume-link" href="#">
