a6d8e21355d91bbe99e288272b2345666018c80e
Josue Jeronimo <josuejero@hotmail.com>
2024-05-09 19:30:15 -0400

    home page updated sorta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,42 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import type { NextPage } from "next";
-import Dice from "@/components/dice"
-import Sidebar from "@/components/sidebar"
+import Dice from "@/components/dice";
+import Sidebar from "@/components/sidebar";
 
-const Home: NextPage = ()=>{
-  return(
+const Home: NextPage = () => {
+  return (
     <div className={styles.home}>
-      <img className={styles.homeIcon} alt="" src="/"/>
+      <Image
+        className={styles.twitterIcon}
+        src="/twitter.svg"
+        alt="Twitter Logo"
+        width={50} // Set appropriate width
+        height={50} // Set appropriate height
+        fill={false} // New prop to replace layout="fixed"
+      />
+      <Image
+        className={styles.linkedinIcon}
+        src="/linkedin.svg"
+        alt="LinkedIn Logo"
+        width={50} // Set appropriate width
+        height={50} // Set appropriate height
+        fill={false} // New prop to replace layout="fixed"
+      />
+      <Image
+        className={styles.githubIcon}
+        src="/github.svg" // Corrected path
+        alt="GitHub Logo"
+        width={50} // Set appropriate width
+        height={50} // Set appropriate height
+        fill={false} // New prop to replace layout="fixed"
+      />
+      <Dice />
+      <Sidebar />
+      <div className={styles.softwareEngineer}>Software Engineer</div>
+      <h2 className={styles.josueJeronimo}>Josué Jerónimo</h2>
     </div>
-  )
+  );
 }
 
 export default Home;
