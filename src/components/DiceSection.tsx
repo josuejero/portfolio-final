74b0f091a981ae36d621a178e474fbc1c64eb4ec
josue <josuejero@hotmail.com>
2024-05-20 11:21:04 -0400

    update

--- src/components/DiceSection.tsx
+++ src/components/DiceSection.tsx
@@ -1,7 +1,27 @@
-export default function DiceSection(){
-  return(
-    <div>
-      
+import type { NextPage } from "next";
+import Image from "next/image";
+import styles from "@/app/styles/DiceSection.module.scss";
+
+const DiceSection: NextPage = () => {
+  return (
+    <div className={styles.diceSection}>
+      <h2 className={styles.discoverMyProjects}>
+        Discover My Projects By Skill!
+      </h2>
+      <div className={styles.diceWrapper}>
+        <Image
+          className={styles.pythonLogoIcon}
+          src="/pythonlogoonly-1@2x.png"
+          alt="Python Logo"
+          width={100} // Adjust width as needed
+          height={100} // Adjust height as needed
+        />
+        <button className={styles.rollDiceButton}>
+          ROLL THE DIE!
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default DiceSection;
